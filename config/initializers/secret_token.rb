8d02c39b6af4e0745f29e66980d1b3813565e17c
The WB <westen@scribd.com>
2013-03-01 11:31:15 -0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mastermind::Application.config.secret_token = '5050a62e37d2cb9c844fcdf02a411650f44b475891659a243c1d6bc5476b1ceed330e8c783321ab040b6ad9c46e3c5ee8b74450bc3ef12cb23ce44075d32be1c'
